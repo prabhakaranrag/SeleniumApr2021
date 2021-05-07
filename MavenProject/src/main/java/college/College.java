c544cb03668aeb5f626618b2f8e9e869044c0810
prabhakaranrag <33579320+prabhakaranrag@users.noreply.github.com>
2021-05-07 17:03:44 -0400

    Add files via upload

---
+++ MavenProject/src/main/java/college/College.java
@@ -0,0 +1,15 @@
+package org.college;
+
+public class College {
+	
+	protected void collegeName() {
+		System.out.println("DGVC");
+	}
+	protected void collegeCode() {
+		System.out.println("1234");
+	}
+	protected void collegeRank() {
+		System.out.println("11");
+	}
+
+}
---
+++ MavenProject/src/main/java/college/Department.java
@@ -0,0 +1,11 @@
+package org.college;
+
+public class Department extends College {
+
+	protected void deptName() {
+		
+		System.out.println("IT");
+	}
+	
+	
+}
